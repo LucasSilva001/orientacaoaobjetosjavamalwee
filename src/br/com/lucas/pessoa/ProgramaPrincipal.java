b1824e4a3649e4bf8484a64355e43e1328a84ef3
Luquinhassim <Lucasdaraelu@gmail.com>
2021-06-07 16:33:21 -0300

    Instância dos objetos Pessoa e PessoaController

--- src/br/com/lucas/pessoa/ProgramaPrincipal.java
+++ src/br/com/lucas/pessoa/ProgramaPrincipal.java
@@ -1,9 +1,43 @@
 package br.com.lucas.pessoa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
-		
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+
+		PessoaController pessoaController = new PessoaController();
+
+		boolean sair = false;
+
+		do {
+			pessoaController.menu();
+
+			int opcao = pessoaController.leOpcao();
+
+			switch (opcao) {
+			case 1:
+				pessoas.add(pessoaController.cadastrarPessoa());
+				break;
+				
+			case 2:
+				pessoaController.ListarPessoas(pessoas);
+				break;
+				
+			case 9:
+				sair = true;
+				break;
+
+			default:
+				System.out.println("Opção inválida!");
+				break;
+			}
+
+		} while (!sair);
+
+		System.out.println("Sistema finalizado!");
 
 	}
 
