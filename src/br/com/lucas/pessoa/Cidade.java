a054e2d0f9dfb29c7af62fa270511d66c31e233c
Luquinhassim <Lucasdaraelu@gmail.com>
2021-06-10 16:37:38 -0300

    Configuração das classes de para herança em Pessoa

---
+++ src/br/com/lucas/pessoa/Cidade.java
@@ -0,0 +1,15 @@
+package br.com.lucas.pessoa;
+
+public class Cidade extends Estado {
+	private String nomeDaCidade;
+
+	public String getNomeDaCidade() {
+		return nomeDaCidade;
+	}
+
+	public void setNomeDaCidade(String nomeDaCidade) {
+		this.nomeDaCidade = nomeDaCidade;
+	}
+	
+	
+}
